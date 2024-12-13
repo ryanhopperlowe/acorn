9e21d43b8be598e0764dbf5d6471a8c2f2b51057
Ryan Hopper-Lowe <ryan@acorn.io>
2024-12-13 10:05:48 -0600

    chore: enable all future flags for upgrade from remix to react-router

--- ui/admin/vite.config.ts
+++ ui/admin/vite.config.ts
@@ -3,6 +3,12 @@ import { remixRoutes } from "remix-routes/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/node" {
+    interface Future {
+        v3_singleFetch: true;
+    }
+}
+
 export default defineConfig({
     plugins: [
         remix({
@@ -11,6 +17,9 @@ export default defineConfig({
                 v3_fetcherPersist: true,
                 v3_relativeSplatPath: true,
                 v3_throwAbortReason: true,
+                v3_lazyRouteDiscovery: true,
+                v3_singleFetch: true,
+                v3_routeConfig: true,
             },
             basename: "/admin/",
         }),
